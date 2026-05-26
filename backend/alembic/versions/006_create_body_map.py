4dbba871bd3503a50fe394a98c385390e6b8fa00
Matheus Ryan <mrysn@ic.ufal.br>
2026-05-26 11:08:40 -0300

    refactor: optimize body area data insertion in migration script
    - Replaced the bulk insert method with a direct SQL execution
    
    Co-authored-by: Rafael Luciano <rafael.ifal2018@gmail.com>

--- backend/alembic/versions/006_create_body_map.py
+++ backend/alembic/versions/006_create_body_map.py
@@ -5,7 +5,6 @@ Revises: 005_create_checkins
 Create Date: 2026-05-26
 """
 
-import uuid
 from collections.abc import Sequence
 
 import sqlalchemy as sa
@@ -199,170 +198,38 @@ def upgrade() -> None:
     op.create_index("ix_body_area_history_body_area_id", "body_area_history", ["body_area_id"])
     op.create_index("ix_body_area_history_snapshot_at", "body_area_history", ["snapshot_at"])
 
-    body_areas = sa.table(
-        "body_areas",
-        sa.column("id", postgresql.UUID(as_uuid=True)),
-        sa.column("code", sa.Text()),
-        sa.column("label", sa.Text()),
-        sa.column("side", sa.Text()),
-        sa.column("system_part", sa.Text()),
-    )
-    op.bulk_insert(
-        body_areas,
-        [
-            _row(
-                "5e5e2316-0fcc-4a3d-a2b4-51b856f6bf26",
-                "left_cheek",
-                "Bochecha esquerda",
-                "left",
-                "head",
-            ),
-            _row(
-                "bcc5f7ce-2632-47f0-adf2-bbf22bd4ec8a",
-                "right_cheek",
-                "Bochecha direita",
-                "right",
-                "head",
-            ),
-            _row("0de315f6-90d4-4637-a81f-ac2f34512bd7", "forehead", "Testa", "center", "head"),
-            _row("760d7f14-e2df-4426-8f85-0f2750f176ef", "nose", "Nariz", "center", "head"),
-            _row(
-                "b2fb1f7f-6542-4f4b-916c-092f95aa0088",
-                "left_ear",
-                "Orelha esquerda",
-                "left",
-                "head",
-            ),
-            _row(
-                "df80aa7b-a0db-48e2-ae9a-662c2fd8a466",
-                "right_ear",
-                "Orelha direita",
-                "right",
-                "head",
-            ),
-            _row("ce426de8-8108-4e4f-b06f-7e6a2177d7ab", "chest", "Tórax", "center", "trunk"),
-            _row(
-                "f49fd957-89a8-46c5-b69f-4296a8f95784",
-                "upper_back",
-                "Dorso superior",
-                "center",
-                "trunk",
-            ),
-            _row("64d0af93-f2d5-484f-a1a6-53d8f3778cad", "abdomen", "Abdômen", "center", "trunk"),
-            _row("15f12935-ad64-44d3-a39a-b2fc1f614469", "lower_back", "Lombar", "center", "trunk"),
-            _row(
-                "af6a2d96-dde2-4bd8-a67e-beb3a4122f4d",
-                "left_shoulder",
-                "Ombro esquerdo",
-                "left",
-                "upper_limb",
-            ),
-            _row(
-                "ef2f6ed8-25d9-4df0-a6e3-83e85b3fd522",
-                "right_shoulder",
-                "Ombro direito",
-                "right",
-                "upper_limb",
-            ),
-            _row(
-                "e4995804-017b-47bd-b91f-7a05eb1f6710",
-                "left_arm",
-                "Braço esquerdo",
-                "left",
-                "upper_limb",
-            ),
-            _row(
-                "95c7800d-0099-4222-b28a-b4f68b80f473",
-                "right_arm",
-                "Braço direito",
-                "right",
-                "upper_limb",
-            ),
-            _row(
-                "f6e8149e-a72f-4c0c-b44a-a7f5a64f7f26",
-                "left_forearm",
-                "Antebraço esquerdo",
-                "left",
-                "upper_limb",
-            ),
-            _row(
-                "2db83785-7f37-4a6f-9fdc-8b4f8ea2cd8d",
-                "right_forearm",
-                "Antebraço direito",
-                "right",
-                "upper_limb",
-            ),
-            _row(
-                "e8b845d9-8d28-4821-8409-f2b46f7d3e71",
-                "left_hand",
-                "Mão esquerda",
-                "left",
-                "upper_limb",
-            ),
-            _row(
-                "4cfc2ea9-729f-45f1-a077-45e814d9ecf2",
-                "right_hand",
-                "Mão direita",
-                "right",
-                "upper_limb",
-            ),
-            _row(
-                "eeb12cd1-e21b-4832-b84f-c87f8dca76f8",
-                "left_thigh",
-                "Coxa esquerda",
-                "left",
-                "lower_limb",
-            ),
-            _row(
-                "9d495f41-6334-4e60-b7ff-ec76349fd319",
-                "right_thigh",
-                "Coxa direita",
-                "right",
-                "lower_limb",
-            ),
-            _row(
-                "6ff9629e-c5c7-4cd2-ac17-95f40eeec54b",
-                "left_knee",
-                "Joelho esquerdo",
-                "left",
-                "lower_limb",
-            ),
-            _row(
-                "c4bbf2ac-d58e-4917-90d3-f8fcd7a68eb8",
-                "right_knee",
-                "Joelho direito",
-                "right",
-                "lower_limb",
-            ),
-            _row(
-                "8f42d97f-cbb8-4c7c-a2f9-dbe463aa6f57",
-                "left_leg",
-                "Perna esquerda",
-                "left",
-                "lower_limb",
-            ),
-            _row(
-                "b1307f4a-1783-47bc-82f8-6d28242ee379",
-                "right_leg",
-                "Perna direita",
-                "right",
-                "lower_limb",
-            ),
-            _row(
-                "337f9fd7-34dc-42e8-ba32-a5751945163b",
-                "left_foot",
-                "Pé esquerdo",
-                "left",
-                "lower_limb",
-            ),
-            _row(
-                "b87c851f-5dcf-4fb7-8e0d-96f1f4d2af8c",
-                "right_foot",
-                "Pé direito",
-                "right",
-                "lower_limb",
-            ),
-        ],
+    op.execute(
+        sa.text(
+            """
+            INSERT INTO body_areas (id, code, label, side, system_part) VALUES
+            ('5e5e2316-0fcc-4a3d-a2b4-51b856f6bf26','left_cheek','Bochecha esquerda','left','head'),
+            ('bcc5f7ce-2632-47f0-adf2-bbf22bd4ec8a','right_cheek','Bochecha direita','right','head'),
+            ('0de315f6-90d4-4637-a81f-ac2f34512bd7','forehead','Testa','center','head'),
+            ('760d7f14-e2df-4426-8f85-0f2750f176ef','nose','Nariz','center','head'),
+            ('b2fb1f7f-6542-4f4b-916c-092f95aa0088','left_ear','Orelha esquerda','left','head'),
+            ('df80aa7b-a0db-48e2-ae9a-662c2fd8a466','right_ear','Orelha direita','right','head'),
+            ('ce426de8-8108-4e4f-b06f-7e6a2177d7ab','chest','Tórax','center','trunk'),
+            ('f49fd957-89a8-46c5-b69f-4296a8f95784','upper_back','Dorso superior','center','trunk'),
+            ('64d0af93-f2d5-484f-a1a6-53d8f3778cad','abdomen','Abdômen','center','trunk'),
+            ('15f12935-ad64-44d3-a39a-b2fc1f614469','lower_back','Lombar','center','trunk'),
+            ('af6a2d96-dde2-4bd8-a67e-beb3a4122f4d','left_shoulder','Ombro esquerdo','left','upper_limb'),
+            ('ef2f6ed8-25d9-4df0-a6e3-83e85b3fd522','right_shoulder','Ombro direito','right','upper_limb'),
+            ('e4995804-017b-47bd-b91f-7a05eb1f6710','left_arm','Braço esquerdo','left','upper_limb'),
+            ('95c7800d-0099-4222-b28a-b4f68b80f473','right_arm','Braço direito','right','upper_limb'),
+            ('f6e8149e-a72f-4c0c-b44a-a7f5a64f7f26','left_forearm','Antebraço esquerdo','left','upper_limb'),
+            ('2db83785-7f37-4a6f-9fdc-8b4f8ea2cd8d','right_forearm','Antebraço direito','right','upper_limb'),
+            ('e8b845d9-8d28-4821-8409-f2b46f7d3e71','left_hand','Mão esquerda','left','upper_limb'),
+            ('4cfc2ea9-729f-45f1-a077-45e814d9ecf2','right_hand','Mão direita','right','upper_limb'),
+            ('eeb12cd1-e21b-4832-b84f-c87f8dca76f8','left_thigh','Coxa esquerda','left','lower_limb'),
+            ('9d495f41-6334-4e60-b7ff-ec76349fd319','right_thigh','Coxa direita','right','lower_limb'),
+            ('6ff9629e-c5c7-4cd2-ac17-95f40eeec54b','left_knee','Joelho esquerdo','left','lower_limb'),
+            ('c4bbf2ac-d58e-4917-90d3-f8fcd7a68eb8','right_knee','Joelho direito','right','lower_limb'),
+            ('8f42d97f-cbb8-4c7c-a2f9-dbe463aa6f57','left_leg','Perna esquerda','left','lower_limb'),
+            ('b1307f4a-1783-47bc-82f8-6d28242ee379','right_leg','Perna direita','right','lower_limb'),
+            ('337f9fd7-34dc-42e8-ba32-a5751945163b','left_foot','Pé esquerdo','left','lower_limb'),
+            ('b87c851f-5dcf-4fb7-8e0d-96f1f4d2af8c','right_foot','Pé direito','right','lower_limb')
+            """
+        )
     )
 
 
@@ -385,19 +252,3 @@ def downgrade() -> None:
     sa.Enum(name="body_finding_type_enum").drop(op.get_bind(), checkfirst=True)
     sa.Enum(name="body_system_part_enum").drop(op.get_bind(), checkfirst=True)
     sa.Enum(name="body_side_enum").drop(op.get_bind(), checkfirst=True)
-
-
-def _row(
-    id_value: str,
-    code: str,
-    label: str,
-    side: str,
-    system_part: str,
-) -> dict[str, object]:
-    return {
-        "id": uuid.UUID(id_value),
-        "code": code,
-        "label": label,
-        "side": side,
-        "system_part": system_part,
-    }
