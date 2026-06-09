aa025b23c2fd4e110eac7ebf67d624ba8efce6ae
Lucas Heron <lsantosanchieta@gmail.com>
2026-06-09 02:30:46 -0300

    fix: apply alembic ruff suggestions

--- backend/alembic/versions/aeb509f804c0_create_daily_medication_progress_table.py
+++ backend/alembic/versions/aeb509f804c0_create_daily_medication_progress_table.py
@@ -6,17 +6,17 @@ Create Date: 2026-06-08 23:56:27.762692
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "aeb509f804c0"
-down_revision: Union[str, None] = "108_patient_health_appointments"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = "108_patient_health_appointments"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
